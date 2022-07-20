d135d0ca6e434bb8d0d5d8288dcfdb043bd4be3c
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-07-20 14:25:50 -0300

    feat: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import { SignOut } from 'phosphor-react'
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Logo</h1>
+
+      <nav aria-label="Principal" className={styles['navigation-links']}>
+        <ul role="list">
+          <li>
+            <a href="#" data-active>
+              Monitor
+            </a>
+          </li>
+          <li>
+            <a href="#">Vídeos</a>
+          </li>
+          <li>
+            <a href="#">Sobre Nós</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+      </nav>
+
+      <button type="button" className={styles['logout-button']}>
+        Sair
+        <SignOut size={24} />
+      </button>
+    </header>
+  )
+}
