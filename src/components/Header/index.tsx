c8bfbd18add2a5130d3e93732602106ec87c9a28
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-07-21 22:16:38 -0300

    feat: container class

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,31 +4,33 @@ import styles from './styles.module.scss'
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1>Logo</h1>
+      <div className="container">
+        <h1>Logo</h1>
 
-      <nav aria-label="Principal" className={styles['navigation-links']}>
-        <ul role="list">
-          <li>
-            <a href="#" data-active>
-              Monitor
-            </a>
-          </li>
-          <li>
-            <a href="#">Vídeos</a>
-          </li>
-          <li>
-            <a href="#">Sobre Nós</a>
-          </li>
-          <li>
-            <a href="#">Contato</a>
-          </li>
-        </ul>
-      </nav>
+        <nav aria-label="Principal" className={styles['navigation-links']}>
+          <ul role="list">
+            <li>
+              <a href="#" data-active>
+                Monitor
+              </a>
+            </li>
+            <li>
+              <a href="#">Vídeos</a>
+            </li>
+            <li>
+              <a href="#">Sobre Nós</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+          </ul>
+        </nav>
 
-      <button type="button" className={styles['logout-button']}>
-        Sair
-        <SignOut size={24} />
-      </button>
+        <button type="button" className={styles['logout-button']}>
+          Sair
+          <SignOut size={24} />
+        </button>
+      </div>
     </header>
   )
 }
