c99e0d4e649cc2366fff5c57b43742e190a6869b
Herbert Carnauba de Souza <80016718+herbertcarnaubadesouza@users.noreply.github.com>
2022-08-25 08:35:43 -0300

    feat: ajusting favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
         </Head>
         <body>
           <Main />
--- src/server/ServerSingleton.ts
+++ src/server/ServerSingleton.ts
@@ -1,4 +1,3 @@
-import dayjs from 'dayjs'
 
 interface DolarResponse {
   USDBRL: {
