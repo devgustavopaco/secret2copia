eb565510c57f39f28e26402d2d1166aa978d2dfe
Herbert Carnauba de Souza <80016718+herbertcarnaubadesouza@users.noreply.github.com>
2022-10-04 11:18:19 -0300

    fix: link

--- src/components/VideosPage/ClassSchedule/index.tsx
+++ src/components/VideosPage/ClassSchedule/index.tsx
@@ -5,43 +5,45 @@ import styles from './styles.module.scss'
 import Link from 'next/link'
 
 interface videoProps {
-  data: Partial<Videos>[]
+  data: Videos[]
 }
 
 export function ClassScheduleComponent({ data }: videoProps) {
   const router = useRouter()
 
-  const { id } = router.query
+  const { id } = router.query as { id: string }
 
   return (
     <div className={styles.cronogramaSection}>
       <h2>Cronograma de aulas</h2>
       <div className={styles.divisor}></div>
       {data.map((item, index) => (
-        <Link href={`/videos/${item.id}`} key={item.id}>
-          <div className={styles.classContainer}>
-            <span className={styles.date}>{item.createdAt?.toString()}</span>+
-            <div
-              className={
-                id == item.id
-                  ? `${styles.classRoomSection} active`
-                  : styles.classRoomSection
-              }
-            >
-              <div className={styles.classRoom}>
-                <div className={styles.classRoomContent}>
-                  <div className={styles.content}>
-                    <CheckCircle size={18} />
-                    <span>Conteúdo liberado</span>
+        <Link href={`/videos/${encodeURIComponent(item.id)}`} key={item.id}>
+          <a>
+            <div className={styles.classContainer}>
+              <span className={styles.date}>{item.createdAt?.toString()}</span>+
+              <div
+                className={
+                  id == item.id
+                    ? `${styles.classRoomSection} active`
+                    : styles.classRoomSection
+                }
+              >
+                <div className={styles.classRoom}>
+                  <div className={styles.classRoomContent}>
+                    <div className={styles.content}>
+                      <CheckCircle size={18} />
+                      <span>Conteúdo liberado</span>
+                    </div>
+                    <h6 className={styles.liveText}>AULA PRÁTICA</h6>
                   </div>
-                  <h6 className={styles.liveText}>AULA PRÁTICA</h6>
                 </div>
+                <span className={styles.className}>
+                  <strong>Aula 0{index + 1}</strong> - {item.title}
+                </span>
               </div>
-              <span className={styles.className}>
-                <strong>Aula 0{index + 1}</strong> - {item.title}
-              </span>
             </div>
-          </div>
+          </a>
         </Link>
       ))}
     </div>
