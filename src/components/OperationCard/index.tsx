54ed86c49eccaa84c6ab8a0e19f6f05ca6275fe3
Herbert Carnauba de Souza <80016718+herbertcarnaubadesouza@users.noreply.github.com>
2022-11-23 15:53:49 -0300

    feat: trying to ajust dollar prince on taxes

--- src/components/OperationCard/index.tsx
+++ src/components/OperationCard/index.tsx
@@ -110,7 +110,7 @@ export function OperationCard({
         <p>
           <span>Taxas</span>
           {percentageFormatter.format(coin.fee)} + R$
-          {(coin.tax * dollarPrice).toFixed(2)}
+          {(!coin.ask.isUSD ? coin.tax * dollarPrice : coin.tax).toFixed(2)}
         </p>
       </div>
 
