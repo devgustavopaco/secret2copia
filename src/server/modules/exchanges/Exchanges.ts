9be19f0912b5dc0f59bf436337f3d0aadc96b899
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2022-12-08 10:25:17 -0300

    feat: removing console.log

--- src/server/modules/exchanges/Exchanges.ts
+++ src/server/modules/exchanges/Exchanges.ts
@@ -63,8 +63,6 @@ export class BinanceStrategy implements ExchangeStrategy {
     )
     const json = (await response.json()) as BinanceOrderbook
 
-    console.log(json)
-
     this.orderbook[pair] = json
 
     return {
