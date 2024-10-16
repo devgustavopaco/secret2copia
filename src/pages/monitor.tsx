886d098d839b62062d5bbdd10de15c7a914a6c56
DiogoDieger <diogaodieger@gmail.com>
2024-10-16 17:53:10 -0300

    back to railway

--- src/pages/monitor.tsx
+++ src/pages/monitor.tsx
@@ -169,7 +169,7 @@ const Monitoring: NextPage<MonitoringProps> = ({
     setCancelToken(newCancelToken);
 
     const res = await axios.get(
-      `https://nigre.avanzzada.tech/orderbook/getPaginated`,
+      `https://nestjs-nigre-production.up.railway.app/orderbook/getPaginated`,
       {
         params: {
           buyExchanges: buyExchangesName?.join(","),
