5c04cbd497cb77c83f5d2634be12e397350b7e6e
DiogoDieger <diogaodieger@gmail.com>
2024-10-17 14:08:56 -0300

    vercel link

--- src/pages/monitor.tsx
+++ src/pages/monitor.tsx
@@ -169,7 +169,7 @@ const Monitoring: NextPage<MonitoringProps> = ({
     setCancelToken(newCancelToken);
 
     const res = await axios.get(
-      `https://nestjs-nigre-production.up.railway.app/orderbook/getPaginated`,
+      `https://nest-js-nigre.vercel.app/orderbook/getPaginated`,
       {
         params: {
           buyExchanges: buyExchangesName?.join(","),
