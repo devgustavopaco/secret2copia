7964e9234ede4065b346755151e24da1254f911f
DiogoDieger <diogaodieger@gmail.com>
2024-10-25 16:01:48 -0300

    change url

--- src/pages/monitor.tsx
+++ src/pages/monitor.tsx
@@ -171,8 +171,8 @@ const Monitoring: NextPage<MonitoringProps> = ({
       userEmail === "gugold@gmail.com"
         ? "https://nest-js-nigre.vercel.app/orderbook/getPaginated"
         : userEmail === "leolimadorea@gmail.com"
-        ? "https://nestjs-nigre-teste.up.railway.app/orderbook/getPaginated" // Replace with the actual URL
-        : "https://nestjs-nigre-production.up.railway.app/orderbook/getPaginated";
+        ? "https://nestjs-nigre-production.up.railway.app/orderbook/getPaginated" // Replace with the actual URL
+        : "https://nestjs-nigre-teste.up.railway.app/orderbook/getPaginated";
     const res = await axios.get(baseURL, {
       params: {
         buyExchanges: buyExchangesName?.join(","),
