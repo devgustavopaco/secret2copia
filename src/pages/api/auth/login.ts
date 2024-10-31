545942ef397e6351585d8f45fd5a6bf9a65af096
DiogoDieger <diogaodieger@gmail.com>
2024-10-31 14:23:05 -0300

    new login route

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,40 +1,44 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../server/db/client";
 import { verifyPassword } from "../utils/verifyPassword";
 
-export async function POST(req: NextRequest) {
-  try {
-    const { name, email, password, action, userId } = await req.json();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      const { name, email, password, action, userId } = req.body;
 
-    const user = await prisma.user.findUnique({
-      where: { email: email },
-    });
+      const user = await prisma.user.findUnique({
+        where: { email: email },
+      });
 
-    if (!user) {
-      return NextResponse.json(
-        { message: "Credenciais inválidas." },
-        { status: 401 }
+      if (!user) {
+        return res.status(401).json({ message: "Credenciais inválidas." });
+      }
+
+      const isPasswordValid = await verifyPassword(
+        password,
+        user.password || ""
       );
-    }
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Credenciais inválidas." });
+      }
 
-    const isPasswordValid = await verifyPassword(password, user.password || "");
-    if (!isPasswordValid) {
-      return NextResponse.json(
-        { message: "Credenciais inválidas." },
-        { status: 401 }
-      );
+      const { password: _, ...userData } = user;
+      return res.status(200).json({
+        message: "Login bem-sucedido.",
+        user: userData,
+      });
+    } catch (error) {
+      console.error("Erro:", error);
+      return res
+        .status(500)
+        .json({ message: "Erro ao processar a solicitação." });
     }
-
-    const { password: _, ...userData } = user;
-    return NextResponse.json({
-      message: "Login bem-sucedido.",
-      user: userData,
-    });
-  } catch (error) {
-    console.error("Erro:", error);
-    return NextResponse.json(
-      { message: "Erro ao processar a solicitação." },
-      { status: 500 }
-    );
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
