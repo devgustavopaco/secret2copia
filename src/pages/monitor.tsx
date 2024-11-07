db8f1c0c5282a73b924f14b64e89a9fa0e45453d
DiogoDieger <diogaodieger@gmail.com>
2024-11-07 10:04:38 -0300

    new url

--- src/pages/monitor.tsx
+++ src/pages/monitor.tsx
@@ -168,7 +168,7 @@ const Monitoring: NextPage<MonitoringProps> = ({
     const newCancelToken = axios.CancelToken.source();
     setCancelToken(newCancelToken);
     const baseURL =
-      "https://nestjs-nigre-app.up.railway.app/orderbook/getPaginated";
+      "https://nestjs-nigre-teste.up.railway.app/orderbook/getPaginated";
     const res = await axios.get(baseURL, {
       params: {
         buyExchanges: buyExchangesName?.join(","),
