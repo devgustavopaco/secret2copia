4df82c50a3274be727c41aa4b3a54b3961ad21da
devgustavopaco <gustavopacosa@gmail.com>
2025-01-27 19:14:55 -0300

    vaii por favor

--- next.config.js
+++ next.config.js
@@ -2,12 +2,25 @@
 const nextConfig = {
   swcMinify: false,
   reactStrictMode: true,
+  compiler: {
+    styledComponents: false,
+    removeConsole: false,
+  },
   experimental: {
     esmExternals: false,
     forceSwcTransforms: false,
+    swcTraceProfiling: false,
+    swcMinifyDebugOptions: {
+      compress: false,
+      mangle: false,
+    },
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
+    config.optimization = {
+      ...config.optimization,
+      minimize: false,
+    };
     return config;
   },
 };
