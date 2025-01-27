07a0b68ad167bf200809b437d42d9ecc02b773b5
devgustavopaco <gustavopacosa@gmail.com>
2025-01-27 19:18:16 -0300

    pacoca

--- next.config.js
+++ next.config.js
@@ -2,25 +2,8 @@
 const nextConfig = {
   swcMinify: false,
   reactStrictMode: true,
-  compiler: {
-    styledComponents: false,
-    removeConsole: false,
-  },
-  experimental: {
-    esmExternals: false,
-    forceSwcTransforms: false,
-    swcTraceProfiling: false,
-    swcMinifyDebugOptions: {
-      compress: false,
-      mangle: false,
-    },
-  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
-    config.optimization = {
-      ...config.optimization,
-      minimize: false,
-    };
     return config;
   },
 };
