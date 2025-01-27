7fd7610a22d1dafeb25f7b29bed28b7c706b7579
devgustavopaco <gustavopacosa@gmail.com>
2025-01-27 19:03:06 -0300

    build 2

--- next.config.js
+++ next.config.js
@@ -6,15 +6,10 @@ const nextConfig = {
     esmExternals: false,
     forceSwcTransforms: false,
   },
-};
-
-module.exports = nextConfig;
-
-module.exports = {
-  webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
-
     return config;
   },
 };
+
+module.exports = nextConfig;
