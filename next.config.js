0b04b43d5f5b62f4490a917d3e305450e1a4fe0b
devgustavopaco <gustavopacosa@gmail.com>
2025-01-27 18:59:27 -0300

    build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
   reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    esmExternals: false,
+    forceSwcTransforms: false,
+  },
 };
 
 module.exports = nextConfig;
