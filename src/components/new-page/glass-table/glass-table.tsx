a22889fab37046007c84b73a413127b938f6aeaa
leolimadorea <leolimadorea@gmail.com>
2025-10-21 17:05:42 -0300

    up

--- src/components/new-page/glass-table/glass-table.tsx
+++ src/components/new-page/glass-table/glass-table.tsx
@@ -1591,7 +1591,7 @@ export function DemoGlassTable({
 
             <div className={styles.pageNumbers}>
               {Array.from({ length: Math.min(5, totalPages) }, (_, i) => {
-                let pageNum;
+                let pageNum: number;
                 if (totalPages <= 5) {
                   pageNum = i + 1;
                 } else if (currentPage <= 3) {
