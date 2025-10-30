6b3a1e5acf6f5ca0500cc7d025c0263f572c1ae4
DiogoDieger <149592347+DiogoDieger@users.noreply.github.com>
2025-10-30 14:16:53 -0300

    up

--- src/hooks/useArbitrageSocket.ts
+++ src/hooks/useArbitrageSocket.ts
@@ -4,7 +4,7 @@ import { io, Socket } from "socket.io-client";
 import { ArbitrageOpportunity } from "../server/router/orderbook";
 
 const URL = "https://futures-socket-production-7788.up.railway.app/";
-
+//
 const keyOf = (opp: ArbitrageOpportunity) =>
   `${opp.ticker}-${opp.lowestAsk.exchange}-${opp.highestBid.exchange}`;
 
