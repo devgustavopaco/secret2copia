5417797d213ed6905956265029439a84d1c8c699
DiogoDieger <149592347+DiogoDieger@users.noreply.github.com>
2025-10-31 13:00:29 -0300

    up

--- src/hooks/useArbitrageSocket.ts
+++ src/hooks/useArbitrageSocket.ts
@@ -4,7 +4,7 @@ import { io, Socket } from "socket.io-client";
 import { ArbitrageOpportunity } from "../server/router/orderbook";
 
 const URL = "https://stable-socket.nextgain.com.br/";
-//
+
 const keyOf = (opp: ArbitrageOpportunity) =>
   `${opp.ticker}-${opp.lowestAsk.exchange}-${opp.highestBid.exchange}`;
 
